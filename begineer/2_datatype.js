4a831f2794b3ba0bc201ad9c6bcac0f34b16e5f4
Bajrang05 <bajrangyelure05@gmail.com>
2025-09-16 05:29:27 +0000

    #

---
+++ begineer/2_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using node js not browser this method only work in browser
+
+let name="code aur chai" //use "" for string it good practice
+let age=6
+let popular=true
+let type=null
+if(age<18){
+    popular=false
+}
+console.table([name,age,popular])
+
+
+
+// number=>2 to power 53
+//bigint
+//string=>"" use
+//null=>standalone value
+//undefined=>abi value asccine nani hui hai
+//symbol=>unique
+
+//object
+
+console.log(typeof(type));
