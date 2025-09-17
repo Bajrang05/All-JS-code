6e569edf7a3d8a5f6d98251e44a86e57277acb58
Bajrang05 <bajrangyelure05>
2025-09-17 20:00:26 +0530

    all string method

--- begineer/4_string.js
+++ begineer/4_string.js
@@ -1,6 +1,35 @@
-const name="thete";
-let age=90;
+//in this lecture only working on different method provided by string
 
-console.log(`Hello my name is ${name} and my age ${age}`);
+//string
+const string="This This was use for practicing";
+let name="anuj";
+let name_='aditya';
+let name__=`om`;
+//this are the diffent method use to create a string 
+let naMe=new String("RAM"); //--->this use to creat object 
 
-const nadme="dkf";
+// console.log(typeof naMe);
+// console.log(typeof name);
+// console.log(typeof name_);
+// console.log(typeof name__);
+
+/* -----------------Methods--------------- */
+
+console.log(name_.length);
+
+console.log(name_.toUpperCase());  //uppercase
+console.log(naMe.toLowerCase());   //losercase
+
+console.log(string.indexOf("was")); //index of
+console.log(string.indexOf("T"));
+console.log(string.lastIndexOf("This")) //last index
+
+console.log(string.includes("pra"));  //check whether present or not
+
+console.log(string.slice(-4,-1));
+console.log(string.substring(0,4));
+console.log(string.substr(4,8));
+
+console.log(name.concat(" ",name_));
+console.log(string.charAt(5));
+console.log(string.replaceAll("This","Anuj"));
