209ce22b67297f847f53ce037c288315596cec3f
Bajrang05 <bajrangyelure05@gmail.com>
2025-09-17 05:37:19 +0000

    #

--- begineer/3_datatype_conversion.js
+++ begineer/3_datatype_conversion.js
@@ -2,10 +2,10 @@
 let score=undefined
 
 let valueInNumber=Number(score)
-console.log(typeof score);
-console.log(typeof valueInNumber);
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 //there is issue in javascript whenever there is typecoverion occur it may not give proper ans
 
@@ -20,7 +20,46 @@ let isPopular=true;
 
 let value=Number(isPopular);
 
-console.table([value,typeof value]);
+// console.table([value,typeof value]);
 
 
 //that for all keep doing and keep exploring
+
+
+
+/*******************operation****************** */
+
+let x=30;
+let y=x++;
+console.log(x+" "+y)
+
+let a=0;
+let b=++a;
+console.log(a,b)
+
+// console.log(null >=0);
+// console.log(null==0);
+
+// console.log(undefined==0);
+// console.log("2"==2);
+
+//dont write the above code it is waste of time and no logic build
+
+let arr=[3,4,5,6,7];
+console.log(typeof arr);
+
+let myFunction=function(){
+    console.log("hello world")
+}
+console.log(typeof myFunction);
+
+let userOne={
+    email:"yelurebajrang@gmail.com",
+    pass:"iiiiiuuuueeee"
+}
+let userTwo=userOne;
+console.log(userOne);
+userTwo.email="bajrangyelure05";
+
+console.log(userOne);
+console.log(userTwo);
---
+++ begineer/4_string.js
@@ -0,0 +1,4 @@
+const name="thete";
+let age=90;
+
+console.log(`Hello my name is ${name} and my age ${age}`);
