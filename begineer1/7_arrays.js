ba5d59c4ed13e7f7b66bcc550751898881ef1988
Bajrang05 <bajrangyelure05>
2025-09-24 19:18:22 +0530

    #

---
+++ begineer1/7_arrays.js
@@ -0,0 +1,20 @@
+const myarr=[1,2,3,4,5,];
+console.log(myarr);
+
+// myarr.push(6);
+// myarr.pop();
+
+myarr.unshift(8);
+myarr.shift()
+
+console.log(myarr.length)
+
+console.log(myarr);
+
+const myn1=myarr.slice(1,3);
+console.log(myn1);
+console.log(myarr)
+
+const myn2=myarr.splice(1,3);
+console.log(myn2);
+console.log(myarr);
