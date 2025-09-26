07d902c579b744c58f603c59fcfb6df1aff10a2d
Bajrang05 <bajrangyelure05>
2025-09-26 11:05:43 +0530

    #

--- begineer2/4_object.js
+++ begineer2/4_object.js
@@ -16,4 +16,16 @@ const newuser={
 // console.log(obj3);
 // console.log(user);
 const obj4={ ...user, ...newuser};
-console.log(obj4);
+// console.log(obj4);
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+
+const {id:user_id,fullname:name,email:user_gmail,gender="male"}=user;
+console.log(user_id);
+console.log(gender);
+console.log(user);
+const {id,email}=newuser;
+console.log(email);
